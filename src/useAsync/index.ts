7d920fd372028ed4e04491df45026548c93d206b
tangzx <tangzx@akulaku.com>
2023-10-18 19:23:36 +0800

    feat: 🎸 add useAsync hook

--- src/useAsync/index.ts
+++ src/useAsync/index.ts
@@ -1,48 +1,74 @@
-// import useAsyncFn from '@/useAsyncFn';
-// import isBoolean from '@/utils/isBoolean';
-// import { DependencyList, useCallback, useEffect, useState } from 'react';
+import { PromiseNoop } from '@/types';
+import useAsyncFn from '@/useAsyncFn';
+import isBoolean from '@/utils/isBoolean';
+import {
+  DependencyList,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 
-// export interface UseAsyncOptions {
-//   manual?: boolean; // default false,
-//   deps?: DependencyList; // deps
-//   defaultParams?: any; // default params, will be used when fn called automatically.
-//   onSuccess?: (value: any) => void; //
-//   onError?: (e: Error, value?: any) => void;
-//   onFinally?: () => void;
-// }
+export interface UseAsyncOptions {
+  manual?: boolean; // default false,
+  deps?: DependencyList; // deps
+  defaultParams?: any; // default params, will be used when fn called automatically.
+  onSuccess?: (value: any) => void; //
+  onError?: (e: Error) => void;
+  onFinally?: (e: Error, value: any) => void;
+  debounce?: number; // debounce wait
+}
 
-// const useAsync = (fn: () => Promise<any>, options: UseAsyncOptions) => {
-//   const [isManual] = useState(
-//     () => isBoolean(options.manual) && options.manual,
-//   );
-//   const fnReult = useAsyncFn(fn, options.deps);
+const useAsync = <T extends PromiseNoop>(fn: T, options?: UseAsyncOptions) => {
+  const _optionsRef = useRef(options);
+  const [isManual] = useState(
+    () => isBoolean(options?.manual) && options?.manual,
+  );
+  useMemo(() => {
+    _optionsRef.current = options;
+  }, [
+    options?.manual,
+    options?.deps,
+    options?.onSuccess,
+    options?.onError,
+    options?.onFinally,
+  ]);
 
-//     const run = useCallback((...args: any) => {
-//       fnReult.run(...args).then((state) => {
-//           return options.onSuccess?.(state.value);
-//       }).catch(() => {
-//           return options.onError?.(state.value);
-//       })
-//     }, [fnReult.run])
+  const fnReult = useAsyncFn(fn, options?.deps);
 
-//   useEffect(() => {});
+  const run = useCallback(
+    (...args: any) => {
+      let _error: Error;
+      let _value: any;
+      fnReult.run
+        .apply(null, args)
+        .then((value) => {
+          _value = value;
+          _optionsRef.current?.onSuccess?.(value);
+        })
+        .catch((error) => {
+          _error = error;
+          _optionsRef.current?.onError?.(error);
+        })
+        .finally(() => {
+          _optionsRef.current?.onFinally?.(_error, _value);
+        });
+    },
+    [fnReult.run],
+  );
 
-//   useEffect(() => {
-//     if (isManual) {
-//       return;
-//     }
-//     run(options.defaultParams);
-//   }, []);
+  useEffect(() => {
+    if (isManual) {
+      return;
+    }
+    run(options?.defaultParams);
+  }, []);
 
-//   useEffect(() => {
+  return {
+    ...fnReult,
+    run,
+  };
+};
 
-//   } , [])
-
-//   return {
-//     ...fnReult,
-//   };
-// };
-
-// export default useAsync;
-
-export {};
+export default useAsync;
